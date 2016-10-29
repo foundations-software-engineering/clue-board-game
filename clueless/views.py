687e9e178d124552c38797e0c925a509993160ff
Jerrold Vincent <JerroldJVincent@gmail.com>
2016-10-29 08:52:40 -0700

    Added template for index view

--- clueless/views.py
+++ clueless/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-	return HttpResponse("Hello World! Welcome to Clue-Less")
-
+	#return HttpResponse("Welcome to the world of Clue-Less!")
+	template = loader.get_template('clueless/index.html')
+	context = {}
+	return HttpResponse(template.render(context, request))
