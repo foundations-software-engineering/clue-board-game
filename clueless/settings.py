99a522c20bede4be8649a4dea2f9407aedeefd97
Jerrold Vincent <JerroldJVincent@gmail.com>
2016-11-01 21:03:49 -0700

    MAJOR changes to models. Minor change to settings import
    Built out all the model classes.  Changed settings_secret to import at
    end, so certain settings could be overriden (e.g. database password).

--- clueless/settings.py
+++ clueless/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-from .settings_secret import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -118,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+from .settings_secret import *
