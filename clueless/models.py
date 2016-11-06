94427f17fdebffa3739c82117a34d9c15cafc85c
Jerrold Vincent <JerroldJVincent@gmail.com>
2016-11-06 12:33:24 -0700

    Ton of model updates
    Implemented the following classes/enums: STATUS_CHOICES, CaseFile.  Made
    changes (a couple relationships, mostly adding empty method stubs) to
    the following classes: Board, Turn, Action, Suggestion, Accusation,
    Move, Game, and DetectiveSheet.

--- clueless/models.py
+++ clueless/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+import datetime
+
+"""
+Implementation of status enum as a Django IntergerField of choices
+"""
+NOT_STARTED = 0
+STARTED = 1
+COMPLETE = 2
+STATUS_CHOICES = (
+    (NOT_STARTED, 'Not Started'),
+    (STARTED, 'Started'),
+    (COMPLETE, 'Complete'),
+)
+
 class Board(models.Model):
     """
     Board object for the entire game.  Should be referenced by multiple games and space collections
@@ -28,8 +42,9 @@ class Player(models.Model):
     Represents a player in the context of a clueless game.  Ties back to Django user
     """
     user = models.ForeignKey(User)
-    currentSpace = models.ForeignKey(Space)
-    currentGame = models.ForeignKey('Game') # game not defined yet, using string as lazy lookup
+    currentSpace = models.ForeignKey(Space, blank=True)
+    currentGame = models.ForeignKey('Game', blank=True) # game not defined yet, using string as lazy lookup
+    character = models.ForeignKey('Character', blank=True)
 
 class Hallway(SpaceCollection):
     """
@@ -114,37 +129,123 @@ class Turn(models.Model):
     """
     player = models.ForeignKey(Player)
     game = models.ForeignKey('Game') #Game class not defined yet, referencing by string
+    status = models.IntegerField(choices=STATUS_CHOICES, default=1)
+
+    def getAvailableActions(self):
+        """
+        :return: Set of Action subclass class objects that can be taken at this point
+        """
+        #TODO: Implement this method
+        return(None)
+
+    def takeAction(self, action):
+        """
+        Performs an action
+        :param action: Subclass of Action, which will have its performAction function called
+        :return:
+        """
+        #TODO: implement this method
+        return(None)
+
+    def endTurn(self):
+        """
+        Ends this turn
+        """
+        pass
 
 class Action(models.Model):
     """
     Any player action a player can take during a turn
     """
-    turn = models.ForeignKey(Turn)
+    turn = models.ForeignKey(Turn, blank=True)
+    description = models.CharField(max_length=255, blank=True)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=1)
+
+    def validate(self):
+        """
+        :return: boolean Can the action be validly taken
+        """
+        raise NotImplementedError("Subclasses of Action need to implement abstract method validate")
+
+    def performAction(self):
+        """
+        Performs the action
+        """
+        raise NotImplementedError("Subclasses of Action need to implement abstract method performAction")
+
 
 class Suggestion(Action):
     """
     A suggestion action taken by the player
     """
-    pass
+    whoWhatWhere = models.ForeignKey(WhoWhatWhere)
 
 class Accusation(Action):
     """
     An accusation action taken by the player.  Either the player wins the game or they lose!
     """
-    pass
+    whoWhatWhere = models.ForeignKey(WhoWhatWhere)
 
 class Move(Action):
     """
     Any move a player makes
     """
-    pass
+    fromSpace = models.ForeignKey(Space, related_name='fromSpace')
+    toSpace = models.ForeignKey(Space, related_name='toSpace')
+
+class CaseFile(WhoWhatWhere):
+    """
+    Class to be used for the secret CaseFile for a Game
+    """
+    @classmethod
+    def createRandom(cls):
+        """
+        Static class method, to be used instead of constructor in most cases
+        :return: CaseFile with random selections for room, character and weapon
+        """
+        #TODO: actually write this function...
+        return(None)
 
 class Game(models.Model):
     """
     Parent game object
     """
-    caseFile = models.ForeignKey(WhoWhatWhere)
+    caseFile = models.ForeignKey(CaseFile)
     board = models.ForeignKey(Board)
+    status = models.IntegerField(choices = STATUS_CHOICES, default = 1)
+    lastUpdateTime = models.DateTimeField(default=datetime.datetime.now, blank=True)
+
+    def startGame(self, playerHost):
+        """
+        Starts a game
+        :param playerHost: player that will be the host
+        """
+        #TODO: implement this method
+        pass
+
+    def addPlayer(self, player):
+        """
+        Adds a player to the game
+        :param player: Player to be added
+        """
+        #TODO: implement this method
+        pass
+
+    def endGame(self, winningPlayer):
+        """
+        Ends the game
+        :param winningPlayer: Player who won
+        """
+        #TODO: implement this method
+        pass
+
+    def loseGame(self, losingPlayer):
+        """
+        Ends the game for a losing player
+        :param losingPlayer: Player who lost (bad accusation
+        """
+        #TODO: implement this method
+        pass
 
 class DetectiveSheet(models.Model):
     """
@@ -153,6 +254,36 @@ class DetectiveSheet(models.Model):
     """
     game = models.ForeignKey(Game)
     player = models.ForeignKey(Player)
+
+    def getRoomsLeft(self):
+        """
+        :return: Set of Room object that a player has not yet checked off
+        """
+        #TODO: implement this method
+        return(None)
+
+    def getCharactersLeft(self):
+        """
+        :return: Set of Character objects that a player has not yet checked off
+        """
+        # TODO: implement this method
+        return (None)
+
+    def getWeaponsLeft(self):
+        """
+        :return: Set of Weapon objects that a player has not yet checked off
+        """
+        # TODO: implement this method
+        return (None)
+
+    def makeNote(self, card, checked):
+        """
+        Notes whether a player has checked off a particular card or not
+        :param card: Card that is being checked off
+        :param checked: boolean whether to check or uncheck
+        """
+        #TODO: implement this method
+        pass
 
 class SheetItem(models.Model):
     """
