9858451fb837b4241a09dc5f095219193ae6b0d5
Rebecca Friedman <rebeccafriedman04@gmail.com>
2016-12-03 15:21:29 -0500

    fixing variables and dynamic dropdown

--- clueless/views.py
+++ clueless/views.py
@@ -171,14 +171,11 @@ def playerturn(request):
 	if request.method == 'POST':
 		if 'user_id' or 'player_move' in request.POST:
 			#store variables for easier usage
-			user_id = request.POST['user_id']
+			user_id = request.user
 			player_move = request.POST['player_move']
 			new_position = request.POST['new_position']
 
-			#TODO: mocking the player for now, but will use database later to get Player once we're actually population it
-			#player = Player.objects.get(user = user_id)
-			player = Player()
-			player.currentPostion = "library"
+			player = Player.objects.get(user = user_id)
 			
 			#redirect to correct page or perform logic check based on choice
 			if player_move == "makeAccusation":
@@ -187,11 +184,11 @@ def playerturn(request):
 				print("redirect to makeAccusation page")
 			elif player_move == "makeSuggestion":
 				#TODO: actually redirect to suggestion page, below code should work once that stuff is merged in
-				#template = loader.get_template('clueless/makeSuggestion.html')
+				#template = loader.get_template('clueless/makeSuggestion.html')w
 				print("redirect to makeSuggestion page")
 			elif player_move == "moveSpace":
 				#TODO: logic for confirming space is valid 
-				print("checking if player " + user_id + " can move to " + new_position + " from " + player.currentPostion)
+				print("checking if player " + str(user_id) + " can move to " + new_position + " from " + str(player.currentSpace))
 		else:
 			#TODO: replace with logging later(don't want to replicate from grehg's but will use below commented out code)
 			#logger.error('user_id or player_move not provided')
